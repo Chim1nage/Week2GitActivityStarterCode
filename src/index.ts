ef4facc8d2642488759d5aec29facdb3c13897ac
Andrew Panos <andrew_panos@me.com>
2023-09-12 12:37:40 -0400

    fixed error in console logs

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,12 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
+console.log(`result_add = ${result_add}`);
 
 const result_mult = calc.multiply(2, 3);
-console.log(`result_add = ${result_add}`);
 console.log(`result_mult = ${result_mult}`);
 
 const result_sub = calc.subtract(7, 3);
-console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
 
 const result_div = calc.divide(9, 3);
